9e2928a8e17d269d43e15235ee25ec71f23b8192
kakashidota <robin.kamo@live.se>
2017-10-17 12:52:55 +0200

    Edit activity

--- app/src/main/java/com/example/kakashi/scorekeeper/IthsFirstActivity.java
+++ app/src/main/java/com/example/kakashi/scorekeeper/IthsFirstActivity.java
@@ -4,17 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 public class IthsFirstActivity extends AppCompatActivity {
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iths_first);
+        editText = (EditText)findViewById(R.id.editText2);
     }
 
     public void sendButton(View view){
         Intent myIntent = new Intent(getApplicationContext(), ithsSecondActivity.class);
+        myIntent.putExtra("Message",editText.getText().toString());
         startActivity(myIntent);
     }
 
--- app/src/main/java/com/example/kakashi/scorekeeper/ithsSecondActivity.java
+++ app/src/main/java/com/example/kakashi/scorekeeper/ithsSecondActivity.java
@@ -1,13 +1,26 @@
 package com.example.kakashi.scorekeeper;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class ithsSecondActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iths_second);
+
+        Intent intent = getIntent();
+
+        String text = intent.getStringExtra("Message");
+        textView = (TextView)findViewById(R.id.textView17);
+        textView.setText(text);
+
     }
+
+
 }
